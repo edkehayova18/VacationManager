cded78d5a9302634ac0edf1fcf22dded2d3ce9ce
edkehayova18 <edkehayova18@codingburgas.bg>
2022-11-14 13:31:40 +0200

    Initial commit

---
+++ src/test/java/com/example/vacationmanager/VacationManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vacationmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
